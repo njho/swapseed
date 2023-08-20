fc46c10dbb53e9bee97e1d41f5158e9cbe1f6036
njho <nj.ho@hotmail.com>
2023-08-20 14:57:03 -0600

    Changes

--- faceswap.py
+++ faceswap.py
@@ -4,41 +4,54 @@ import matplotlib.pyplot as plt
 
 from face_enhancer import load_face_enhancer_model
 
+
 def validate_image(img):
     if not os.path.exists(img):
-        raise ValueError(f'Image {img} does not exist')
-    # check if img is a valid image file 
+        raise ValueError(f"Image {img} does not exist")
+    # check if img is a valid image file
     if not os.path.isfile(img):
-        raise ValueError(f'Image {img} is not a valid image file')
-    # validate it to be jpg jpeg, png formats 
-    if not img.lower().endswith(('.jpg', '.jpeg', '.png')):
-        raise ValueError(f'Image {img} is not a valid image file')
+        raise ValueError(f"Image {img} is not a valid image file")
+    # validate it to be jpg jpeg, png formats
+    if not img.lower().endswith((".jpg", ".jpeg", ".png")):
+        raise ValueError(f"Image {img} is not a valid image file")
+
 
 def cpu_warning(device):
     if device == "cpu":
-        print("Using CPU for face enhancer. If you have a GPU, you can set device='cuda' to speed up the process. You can also set enhance=False to skip the enhancement.")
+        print(
+            "Using CPU for face enhancer. If you have a GPU, you can set device='cuda' to speed up the process. You can also set enhance=False to skip the enhancement."
+        )
 
-def swap_n_show(img1_fn, img2_fn, app, swapper,
-                plot_before=False, plot_after=True, enhance=False, enhancer='REAL-ESRGAN 2x',device="cpu"):
-    
+
+def swap_n_show(
+    img1_fn,
+    img2_fn,
+    app,
+    swapper,
+    plot_before=False,
+    plot_after=True,
+    enhance=False,
+    enhancer="REAL-ESRGAN 2x",
+    device="cpu",
+):
     validate_image(img1_fn)
     validate_image(img2_fn)
-    
+
     img1 = cv2.imread(img1_fn)
     img2 = cv2.imread(img2_fn)
-    
+
     if plot_before:
         fig, axs = plt.subplots(1, 2, figsize=(10, 5))
-        axs[0].imshow(img1[:,:,::-1])
-        axs[0].axis('off')
-        axs[1].imshow(img2[:,:,::-1])
-        axs[1].axis('off')
+        axs[0].imshow(img1[:, :, ::-1])
+        axs[0].axis("off")
+        axs[1].imshow(img2[:, :, ::-1])
+        axs[1].axis("off")
         plt.show()
-    
+
     # Do the swap
     face1 = app.get(img1)[0]
     face2 = app.get(img2)[0]
-    
+
     img1_ = img1.copy()
     img2_ = img2.copy()
     if plot_after:
@@ -46,79 +59,94 @@ def swap_n_show(img1_fn, img2_fn, app, swapper,
         img2_ = swapper.get(img2_, face2, face1, paste_back=True)
         if enhance:
             cpu_warning(device)
-            model, model_runner = load_face_enhancer_model(enhancer,device)
+            model, model_runner = load_face_enhancer_model(enhancer, device)
             img1_ = model_runner(img1_, model)
             img2_ = model_runner(img2_, model)
         fig, axs = plt.subplots(1, 2, figsize=(10, 5))
-        axs[0].imshow(img1_[:,:,::-1])
-        axs[0].axis('off')
-        axs[1].imshow(img2_[:,:,::-1])
-        axs[1].axis('off')
+        axs[0].imshow(img1_[:, :, ::-1])
+        axs[0].axis("off")
+        axs[1].imshow(img2_[:, :, ::-1])
+        axs[1].axis("off")
         plt.show()
     return img1_, img2_
 
-def swap_n_show_same_img(img1_fn,
-                         app, swapper,
-                         plot_before=False,
-                         plot_after=True, enhance=False, enhancer='REAL-ESRGAN 2x',device="cpu"):
-    
+
+def swap_n_show_same_img(
+    img1_fn,
+    app,
+    swapper,
+    plot_before=False,
+    plot_after=True,
+    enhance=False,
+    enhancer="REAL-ESRGAN 2x",
+    device="cpu",
+):
     validate_image(img1_fn)
     img1 = cv2.imread(img1_fn)
-    
+
     if plot_before:
         fig, ax = plt.subplots(1, 1, figsize=(10, 5))
-        ax.imshow(img1[:,:,::-1])
-        ax.axis('off')
+        ax.imshow(img1[:, :, ::-1])
+        ax.axis("off")
         plt.show()
-    
+
     # Do the swap
     faces = app.get(img1)
     face1, face2 = faces[0], faces[1]
-    
+
     img1_ = img1.copy()
     if plot_after:
         img1_ = swapper.get(img1_, face1, face2, paste_back=True)
         img1_ = swapper.get(img1_, face2, face1, paste_back=True)
         if enhance:
             cpu_warning(device)
-            model, model_runner = load_face_enhancer_model(enhancer,device)
+            model, model_runner = load_face_enhancer_model(enhancer, device)
             img1_ = model_runner(img1_, model)
         fig, ax = plt.subplots(1, 1, figsize=(10, 5))
-        ax.imshow(img1_[:,:,::-1])
-        ax.axis('off')
+        ax.imshow(img1_[:, :, ::-1])
+        ax.axis("off")
         plt.show()
     return img1_
 
-def swap_face_single(img1_fn, img2_fn, app, swapper,
-             plot_before=False, plot_after=True, enhance=False, enhancer='REAL-ESRGAN 2x',device="cpu"):
-    
-    validate_image(img1_fn)
-    validate_image(img2_fn)
-    
-    img1 = cv2.imread(img1_fn)
-    img2 = cv2.imread(img2_fn)
-    
+
+def swap_face_single(
+    target_img,
+    source_img,
+    app,
+    swapper,
+    plot_before=False,
+    plot_after=True,
+    enhance=False,
+    enhancer="REAL-ESRGAN 2x",
+    device="cpu",
+):
+    validate_image(target_img)
+    validate_image(source_img)
+
+    img1 = cv2.imread(target_img)
+    img2 = cv2.imread(source_img)
+
     if plot_before:
         axs = plt.subplots(1, 2, figsize=(10, 5))
-        axs[0].imshow(img1[:,:,::-1])
-        axs[0].axis('off')
-        axs[1].imshow(img2[:,:,::-1])
-        axs[1].axis('off')
+        axs[0].imshow(img1[:, :, ::-1])
+        axs[0].axis("off")
+        axs[1].imshow(img2[:, :, ::-1])
+        axs[1].axis("off")
         plt.show()
-    
+
     # Do the swap
     face1 = app.get(img1)[0]
     face2 = app.get(img2)[0]
-    
+
     img1_ = img1.copy()
     if plot_after:
         img1_ = swapper.get(img1_, face1, face2, paste_back=True)
         if enhance:
             cpu_warning(device)
-            model, model_runner = load_face_enhancer_model(enhancer,device)
+            model, model_runner = load_face_enhancer_model(enhancer, device)
             img1_ = model_runner(img1_, model)
         # Save the image
-        output_fn = os.path.join('outputs', os.path.basename(img1_fn))
+        output_fn = os.path.join("outputs", os.path.basename(target_img))
         cv2.imwrite(output_fn, img1_)
-        print(f'Image saved to {output_fn}')
+        print(f"Image saved to {output_fn}")
     return img1_
--- main.py
+++ main.py
@@ -1,31 +1,39 @@
 import os
-import matplotlib.pyplot as plt
 import gdown
 import insightface
 from insightface.app import FaceAnalysis
-from insightface.data import get_image as ins_get_image
-from faceswap import swap_n_show, swap_n_show_same_img, swap_face_single
+from faceswap import swap_face_single
 
-app = FaceAnalysis(name='buffalo_l')
+app = FaceAnalysis(name="buffalo_l")
 app.prepare(ctx_id=0, det_size=(640, 640))
 
 # Download 'inswapper_128.onnx' file using gdown
-model_url = 'https://drive.google.com/uc?id=1HvZ4MAtzlY74Dk4ASGIS9L6Rg5oZdqvu'
-model_output_path = 'inswapper/inswapper_128.onnx'
+model_url = "https://drive.google.com/uc?id=1HvZ4MAtzlY74Dk4ASGIS9L6Rg5oZdqvu"
+model_output_path = "inswapper/inswapper_128.onnx"
 if not os.path.exists(model_output_path):
     gdown.download(model_url, model_output_path, quiet=False)
 
-swapper = insightface.model_zoo.get_model('inswapper/inswapper_128.onnx', download=False, download_zip=False)
+swapper = insightface.model_zoo.get_model(
+    "inswapper/inswapper_128.onnx", download=False, download_zip=False
+)
 
 # Load images
-img1_fn = 'images/Anushka.jpg'
-img2_fn = 'images/keerthi.jpg'
+target_img = "images/man_target.jpeg"
+source_img = "images/woman1.jpeg"
 
 # Swap faces between two images
-# swap_n_show(img1_fn, img2_fn, app, swapper)
+# swap_n_show(target_img, source_img, app, swapper)
 
-# Swap faces within the same image 
-# swap_n_show_same_img(img1_fn, app, swapper)
+# Swap faces within the same image
+# swap_n_show_same_img(target_img, app, swapper)
 
 # Add face to an image
-swap_face_single(img1_fn, img2_fn, app, swapper, enhance=True, enhancer='REAL-ESRGAN 2x',device="cpu")
+swap_face_single(
+    target_img,
+    source_img,
+    app,
+    swapper,
+    enhance=True,
+    enhancer="REAL-ESRGAN 2x",
+    device="cpu",
+)
